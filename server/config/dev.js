64df8e626575e287498a3a884dd5814b3975e611
happpyhorse <mmxinran@gmail.com>
2019-10-27 15:55:28 -0700

    set up dev&prod

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+// don't commit IT
+module.exports = {
+    googleClientID: '503035697050-csfbkh032ian5pdfsp0124uddjt1vmjh.apps.googleusercontent.com',
+    googleClientSecret: '6uSxIx-WIxiomTq8KTh-TdGa',
+    mongoURI:' mongodb+srv://xinran:M3qSGREEXZRyqqjQ@blog-dggzx.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey:'a;dfjksaeow;p80:Pklejr'
+}
