ce8cdef2194b60638464806ec0fd53cf55f81897
happpyhorse <mmxinran@gmail.com>
2019-11-03 13:54:06 -0800

    applies style to header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,8 +1,17 @@
 import React, {Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component{
     render(){
-
+        return (
+            <nav>
+                <div className="nav-wrapper">
+                    <Link to='/' className="left brand-logo">Xinran</Link>
+                    <ul className="right">
+                    </ul>
+                </div>
+            </nav>
+        );
     }
 }
 
