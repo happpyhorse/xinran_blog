b627134c5ea1f3b59ed0a5a4726d826e6c6d09c8
happpyhorse <mmxinran@gmail.com>
2019-12-29 21:01:28 -0800

    develops post reducer

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-import { FETCH_USER, CREATE_POST } from './types';
+import {
+    FETCH_USER,
+    CREATE_POST,
+    FETCH_POST,
+    FETCH_POSTS,
+    DELETE_POST,
+    EDIT_POST
+} from './types';
 import posts from './../api/posts'
 
 export const fetchUser = () =>
@@ -17,3 +24,23 @@ export const createPost = formValues =>
         dispatch({ type: CREATE_POST, payload: res.data });
 
     }
+
+export const fetchPosts = () => async dispatch => {
+    const res = await posts.get('/');
+    dispatch({ type: FETCH_POSTS, payload: res.data });
+}
+
+export const fetchPost = (id) => async dispatch => {
+    const res = await posts.get(`/${id}`);
+    dispatch({ type: FETCH_POST, payload: res.data });
+}
+
+export const editPost = (id, formValues) => async dispatch => {
+    const res = await posts.put(`/${id}`, formValues);
+    dispatch({ type: EDIT_POST, payload: res.data });
+}
+
+export const deletePost = (id) => async dispatch => {
+    await posts.delete(`/${id}`);
+    dispatch({ type: DELETE_POST, payload: id });
+}
--- server/client/src/actions/types.js
+++ server/client/src/actions/types.js
@@ -1,3 +1,7 @@
 export const FETCH_USER = 'fetch_user';
 export const FETCH_SURVEYS = 'fetch_surveys';
 export const CREATE_POST = 'create_post';
+export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
+export const EDIT_POST = 'edit_post';
